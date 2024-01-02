a32aef8fd5e930c7fbdd09fce3aa6ee7bfa06bd4
Mohammad Taqui Siddiqui <mtaquisiddiqui@prologis.com>
2024-01-02 17:20:42 +0530

    Animations when page reloaded - Heading and top image

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Hero.css";
 import { HiLocationMarker } from "react-icons/hi";
 import CountUp from "react-countup";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
@@ -12,15 +13,26 @@ const Hero = () => {
         <div className="flexColStart hero-left">
           <div className="hero-title">
             <div className="orange-circle" />
-            <h1>
+            <motion.h1
+              initial={{ y: "2rem", opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{
+                duration: 2,
+                transition: "spring",
+              }}
+            >
               Discover <br />
               Most Suitable <br />
               Property
-            </h1>
+            </motion.h1>
           </div>
           <div className="flexColStart hero-des">
-            <span className="secondaryText">Find a variety of property that suit you very easily</span>
-            <span className="secondaryText" >Forget all difficulties in finding a residence for you</span>
+            <span className="secondaryText">
+              Find a variety of property that suit you very easily
+            </span>
+            <span className="secondaryText">
+              Forget all difficulties in finding a residence for you
+            </span>
           </div>
           <div className="flexCenter search-bar">
             <HiLocationMarker color="var(--blue)" size={25} />
@@ -53,9 +65,17 @@ const Hero = () => {
         </div>
         {/* Right Side*/}
         <div className="flexCenter hero-right">
-          <div className="image-container">
+          <motion.div
+            initial={{ x: "7rem", opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{
+              duration: 2,
+              transition: "spring",
+            }}
+            className="image-container"
+          >
             <img src="./hero-image.png" alt="" />
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
